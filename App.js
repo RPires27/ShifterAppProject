e9139e89356029971cc821e99f2dd6dab845e057
Pires <pirex02@gmail.com>
2022-11-30 23:59:21 +0000

    format

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Text, TextInput, View } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import Toast from 'react-native-toast-message';
 
 function App() {
-
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
 
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
@@ -31,26 +30,35 @@ function App() {
   if (!user) {
     return (
       <NavigationContainer>
-  <Stack.Navigator>
-    <Stack.Screen name="Login" component={Login}options = {{headerShown:false}}/>
-    <Stack.Screen name="Register" component={Register} options = {{headerShown:false}}/>
-  </Stack.Navigator>
-  <Toast></Toast>
-
-  </NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+        <Toast></Toast>
+      </NavigationContainer>
     );
   }
 
   return (
     <NavigationContainer>
-  <Stack.Navigator>
-    <Stack.Screen name="Home" component={Home}options = {{headerShown:false}}/>
-  </Stack.Navigator>
-  <Toast></Toast>
-
-  </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+      <Toast></Toast>
+    </NavigationContainer>
   );
-
 }
 
-export default App
+export default App;
--- screens/Home.js
+++ screens/Home.js
@@ -1,60 +1,50 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
-
 const Home = () => {
-
-  
-
-  const [nome, setnome] = useState("")
+  const [nome, setnome] = useState('');
 
   const SignOut = () => {
     auth()
-    .signOut()
-    .then(() => console.log('User signed out!'));
-  }
+      .signOut()
+      .then(() => console.log('User signed out!'));
+  };
 
   useEffect(() => {
+    firestore()
+      .collection('users')
+      .doc(auth().currentUser.uid)
+      .get()
+      .then(snapshot => {
+        if (snapshot.exists) {
+          setnome(snapshot.data());
+        }
+      });
+  });
 
-    firestore().collection('users')
-    .doc(auth().currentUser.uid)
-    .get()
-    .then((snapshot) => {
-      if(snapshot.exists){
-        setnome(snapshot.data())
-      }
-    })
-    
-  })
-  
-  
   return (
-    <View style = {styles.container}>
-      <Text style ={styles.text}>Olá {nome.nome}!</Text>
-      <Button onPress={SignOut} title='signout'>
-      </Button>
+    <View style={styles.container}>
+      <Text style={styles.text}>Olá {nome.nome}!</Text>
+      <Button onPress={SignOut} title="signout"></Button>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-
-
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor: '#EEC373'
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#EEC373',
   },
-
 
   text: {
-      color:'black',
-      fontFamily:'Quicksand-Bold',
-      fontSize:30,
+    color: 'black',
+    fontFamily: 'Quicksand-Bold',
+    fontSize: 30,
   },
-})
+});
